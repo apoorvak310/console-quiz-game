a60150dfc76137445b561eb5cac52d94c279c40a
Apoorva K <apoorvak310@gmail.com>
2026-09-07 16:33:20 +0530

    Refactor math quiz game structure and logic
    Refactored the math quiz game to improve structure and functionality. Added functions for checking answers, printing options, and handling game flow.

--- console_qz_game.py
+++ console_qz_game.py
@@ -42,42 +42,86 @@ quiz = {
         "answer": "c"
     }
 }
-score = 0
+attempted = unattempted = score = 0
+
 def print_opt(l):
-    o = ord('a')
-    for i in l:
-        print(chr(o),".",end=" ")
-        yield i
-        o += 1
+    for i,o in enumerate(l):
+        print(chr(ord('a') + i),".",o)
+    print("Skip")
+
+def check_answer(a,key):
+    if a == quiz[key]['answer']:
+        return True
+
+def result(f:bool):
+    global score
+    if f == True:
+        score += 1
+    return score
+
+def ask_again(st):
+    if st == 'yes':
+        return True
+    elif st == 'no':
+        return False
+    else:
+        c = input("Choose any one - Yes/No").strip().lower()
+        return ask_again(c)
+
+def play_game(quiz):
+    global attempted
+    global unattempted
+    for k,v in quiz.items():
+        i = 1
+        print(i,')',k)
+        i += 1
+        print_opt(v["options"])
+        ans = input("Answer__").strip().lower()
+        if ans in ['a','b','c','d']:
+            flag = check_answer(ans, k)
+            attempted += 1
+            scr = result(flag)
+        elif ans == 'skip':
+            unattempted += 1
+            continue
+        elif ans == 'e':
+            fl = ask_again(ans)
+            if fl == False:
+                print("Exited")
+                break
+            elif fl == True:
+                continue
+        else:
+            print("Enter a valid option")
+    return scr
 
 print(f"Total Questions: {len(quiz)}")
-choice = input("Shall we start..? (Type Yes (or) No) ").strip().lower()
-if choice == "yes":
-    print("Your time starts now ... if you want to exit, press E")
-    startTime = time.time()
-    for k,v in quiz.items():
-        print(k)
-        for i in print_opt(v["options"]):
-            print(i)
-        ans = input("Answer...").strip().lower()
-        if ans == v["answer"]:
-            score += 1
-            print("Correct!")
-        elif ans == 'e':
-            ch = input("Are you you want to sure to exit ? (yes/no) ").strip().lower()
-            if ch == 'yes':
-                break
-            elif ch == 'no':
-                pass
-        else:
-            print("Wrong answer. The correct answer is:", v["answer"])
-    endTime = time.time()
-    total_seconds = endTime - startTime
-    minutes = int(total_seconds // 60)
-    seconds = int(total_seconds % 60)
-    print(f"Your total Score out of {len(quiz)} is: {score}")
-    print(f"Total time taken: {minutes} minutes.{seconds} seconds")
-elif choice == "no":
+choice = input("Start the game..? (Type Yes (or) No) ").strip().lower()
+if choice == "no":
     print("Okayy, see you next time...")
+elif choice == "yes":
+    # print("Your time starts now ... if you want to exit, press E")
+    # start_time = time.time()
+    score = play_game(quiz)
+    # end_time = time.time()
+    # total_seconds = end_time - start_time
+    # minutes = int(total_seconds // 60)
+    # seconds = int(total_seconds % 60)
+    print("Quiz completed!")
+    print(f"Total Questions Attempted: {attempted}\nTotal questions unattempted: {unattempted}")
+    print(f"Your total Score is {score}/{len(quiz)}")
+   
+while True:
+    again = input("Want to try again? (Yes/No) ").strip().lower()
+    f = ask_again(again)
+    if f == False:
+        print("Exited")
+        break
+    elif f == True:
+        play_game(quiz)
+    #print(f"Total time taken: {minutes}.{seconds:02d} minutes")
 else:
     print("Sorry can't get you :(")
+
+    
+'''leaving this at exit functioning'''
